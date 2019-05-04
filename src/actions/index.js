95011339308e23b249d8d892c93812a21f44c5b1
Regina Yan <regina.l.yan.19@dartmouth.edu>
2019-05-04 00:48:15 -0400

    updated lab5

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@ export const ActionTypes = {
   // CREATE_POST: 'CREATE_POST',
   // DELETE_POST: 'DELETE_POST',
 };
-const ROOT_URL = 'https://lab5-regina-yan-1.herokuapp.com';
+const ROOT_URL = 'https://lab5-regina-yan-1.herokuapp.com/api';
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
 const API_KEY = '?key=r_yan';
 
