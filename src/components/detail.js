88ed89e43f16eda0392d8ebb8fdf7e1758885bf5
Regina Yan <regina.l.yan.19@dartmouth.edu>
2019-05-21 00:49:58 -0400

    added Timeline Detail styling

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,14 +1,29 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 
-const Detail = (props) => {
-  return (
-    <div>
-      {props.title}
-      <br />
-      <br />
-      {props.content}
-    </div>
-  );
-};
+class Detail extends Component {
+//   displayEvents() {
+//     this.props.events.forEach((element) => {
+//       console.log(`element here${element.title}`);
+//       return element.title;
+//     });
+//   }
+
+  render() {
+    return (
+      <div className="detailContainer">
+        <div className="detailTitle">
+          {this.props.title}
+        </div>
+        <div className="detailImg">
+          <img className="Img" src={this.props.cover_url} alt="Detail" />
+        </div>
+        <div className="detailContent">
+          {this.props.content}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Detail;
