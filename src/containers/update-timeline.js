df762f82cf5845903901152dd303ef8e4e9ac095
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-05-21 16:36:38 -0400

    added update route and functionality

---
+++ src/containers/update-timeline.js
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+// import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { updateTimeline } from '../actions';
+import AddForm from '../components/add';
+
+const mapStateToProps = state => (
+  {
+    timeline: state.selected,
+    update: true,
+  }
+);
+
+
+export default withRouter(connect(mapStateToProps, { createTimeline: updateTimeline })(AddForm));
