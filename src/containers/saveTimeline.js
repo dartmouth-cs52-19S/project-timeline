84aaa3b7c1ccf1eb92ce7aa601ddc1b2133f7727
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-05-22 01:13:56 -0400

    Created saveTimeline component to save user paths

---
+++ src/containers/saveTimeline.js
@@ -0,0 +1,80 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+// import Main from '../components/main';
+import TimeElement from './time-element';
+import TimeDetail from './time-detail';
+import { fetchTimeline, saveToTimeline } from '../actions';
+
+
+class SaveTimeline extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currTimeline: this.props.selected,
+    };
+  }
+
+  componentDidMount = () => {
+    if (this.props.selected !== 0 && this.state.currTimeline !== this.props.selected) {
+      this.props.saveToTimeline(this.props.selected);
+      this.setState({ currTimeline: this.props.selected });
+    }
+  }
+
+  componentDidUpdate = () => {
+    if (this.props.selected !== 0 && this.state.currTimeline !== this.props.selected) {
+      this.props.saveToTimeline(this.props.selected);
+      this.setState({ currTimeline: this.props.selected });
+    }
+  }
+
+  render() {
+    console.log(`state of selected is${this.props.selected}`);
+    if (this.props.selected === 0) {
+      return (
+        <div>
+          <div className="foodHeader">
+        Discover the Possibilities
+          </div>
+          <div className="flex">
+            <div className="flex-detail" />
+            <div className="flex-main">
+              <TimeElement />
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div className="foodHeader">
+        Discover the Possibilities
+        </div>
+        <div className="flex">
+          <div className="flex-detail">
+            <TimeDetail />
+          </div>
+          <div className="flex-main">
+            <TimeElement />
+          </div>
+        </div>
+      </div>
+
+
+    );
+  }
+}
+
+const mapStateToProps = state => (
+  {
+    timeline: state.timeline,
+    selected: state.selected,
+  }
+);
+
+
+export default withRouter(connect(mapStateToProps,
+  { fetchTimeline, saveToTimeline })(SaveTimeline));
