d016381e0336abaad01b3f76fa1ad022e5718be7
Katie Goldstein <katie.e.goldstein.20@dartmouth.edu>
2019-05-22 16:49:44 -0400

    user hs graduation date frontend

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -11,11 +11,13 @@ class SignUp extends Component {
       username: '',
       email: '',
       password: '',
+      startTime: '',
     };
 
     this.edit = this.edit.bind(this);
     this.onCancel = this.onCancel.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.createStartTime = this.createStartTime.bind(this);
   }
 
   onCancel(event) {
@@ -28,9 +30,25 @@ class SignUp extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    // error checks
+    if (this.state.email === '') console.log('Please enter an email');
+    if (this.state.username === '') console.log('Please enter a username');
+    if (this.state.password === '') console.log('Please enter a password');
+    if (this.state.startTime === null || Number.isNaN(Date.parse(this.state.startTime))) {
+      console.log('Please enter a valid date');
+    } else {
     // console.log(`sign up info:
     // ${this.state.username} ${this.state.email} ${this.state.password}`);
-    this.props.signupUser(this.state, this.props.history);
+      this.createStartTime();
+      this.props.signupUser(this.state, this.props.history);
+    }
+  }
+
+  createStartTime() { // changes the expected HS grad date to a unix string
+    this.setState((prevState) => {
+      const newStateStart = (new Date((prevState.startTime))).getTime();
+      this.startTime = newStateStart;
+    });
   }
 
   render() {
@@ -50,9 +68,16 @@ class SignUp extends Component {
         />
         <input
           name="password"
+          type="password"
           placeholder="password"
           onChange={this.edit}
           value={this.state.password}
+        />
+        <input
+          name="startTime"
+          placeholder="expected high school graduation YYYY-MM-DD"
+          onChange={this.edit}
+          value={this.state.startTime}
         />
         <button type="button" onClick={this.onCancel}>Cancel</button>
         <button type="button" onClick={this.handleSubmit}>Join</button>
