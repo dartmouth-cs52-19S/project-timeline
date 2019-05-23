5767062e7165d4c57ba9cbc140ec86b78ae0f512
Regina Yan <regina.l.yan.19@dartmouth.edu>
2019-05-22 20:26:27 -0400

    styled sign in and up

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -28,35 +28,73 @@ class SignUp extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // console.log(`sign up info:
-    // ${this.state.username} ${this.state.email} ${this.state.password}`);
     this.props.signupUser(this.state, this.props.history);
   }
 
   render() {
     return (
-      <div className="signin">
-        <input
-          name="username"
-          placeholder="username"
-          onChange={this.edit}
-          value={this.state.username}
-        />
-        <input
-          name="email"
-          placeholder="email"
-          onChange={this.edit}
-          value={this.state.email}
-        />
-        <input
-          name="password"
-          placeholder="password"
-          onChange={this.edit}
-          value={this.state.password}
-        />
-        <button type="button" onClick={this.onCancel}>Cancel</button>
-        <button type="button" onClick={this.handleSubmit}>Join</button>
+      <div className="flexWide">
+        <div className="userGraphic" />
+        <div className="signContainer">
+          <div className="whiteBox">
+            <div className="signin">
+              <div className="signText">
+                <h3>Sign Up</h3>
+                <h4>Sign up and discover all the possibilities in life!</h4>
+              </div>
+              <div className="flexWide">
+                <i className="far fa-user signicon" />
+                <input
+                  name="username"
+                  className="signinput"
+                  placeholder="username"
+                  onChange={this.edit}
+                  value={this.state.username}
+                />
+              </div>
+              <div className="flexWide">
+                <i className="far fa-paper-plane signicon" />
+                <input
+                  name="email"
+                  className="signinput"
+                  placeholder="email"
+                  onChange={this.edit}
+                  value={this.state.email}
+                />
+              </div>
+              <div className="flexWide">
+                <i className="fas fa-lock signicon" />
+                <input
+                  name="password"
+                  className="signinput"
+                  placeholder="password"
+                  onChange={this.edit}
+                  value={this.state.password}
+                />
+              </div>
+              <div className="signSubmitBox">
+                <button
+                  type="button"
+                  className="buttonSecondary"
+                  onClick={this.onCancel}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="buttonCTA"
+                  onClick={this.handleSubmit}
+                >
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
+
+
     );
   }
 }
