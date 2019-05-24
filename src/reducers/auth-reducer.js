19038c216142943d1d467bd762da983835544552
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-05-23 23:31:37 -0400

    Added action and reducers for checking username

--- src/reducers/auth-reducer.js
+++ src/reducers/auth-reducer.js
@@ -3,16 +3,22 @@ import { ActionTypes } from '../actions';
 const initialState = {
   authenticated: false,
   user: {},
+  chkUsername: false,
 };
 
 const AuthReducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.AUTH_USER:
-      return Object.assign({}, state, { authenticated: true, user: action.payload });
+      return { ...state, authenticated: true, user: action.payload };
+      // return Object.assign({}, state, { authenticated: true, user: action.payload });
     case ActionTypes.DEAUTH_USER:
-      return Object.assign({}, state, { authenticated: false, user: {} });
+      return { ...state };
+      // return Object.assign({}, state, { authenticated: false, user: {} });
     case ActionTypes.AUTH_ERROR:
-      return Object.assign({}, state, { authenticated: false, user: {} });
+      return { ...state };
+      // return Object.assign({}, state, { authenticated: false, user: {} });
+    case ActionTypes.CHECK_NAME:
+      return { ...state, chkUsername: action.payload };
     default:
       return state;
   }
