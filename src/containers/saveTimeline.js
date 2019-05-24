0a734f27a25581a928e87ce9c9c1721c3427d951
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-05-23 21:40:51 -0400

    Commented stuff out

--- src/containers/saveTimeline.js
+++ src/containers/saveTimeline.js
@@ -21,18 +21,18 @@ class SaveTimeline extends Component {
   componentDidMount = () => {
     console.log('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH');
 
-    if (this.props.selected !== 0 && this.state.currTimeline !== this.props.selected) {
-      console.log('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH');
+    // if (this.props.selected !== 0 && this.state.currTimeline !== this.props.selected) {
+    console.log('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH');
 
-      console.log('saving');
-      this.props.saveToTimeline(this.props.selected.id);
-      this.setState({ currTimeline: this.props.selected });
-    }
+    console.log('saving');
+    this.props.saveToTimeline(this.props.selected.id);
+    // this.setState({ currTimeline: this.props.selected });
+    // }
   }
 
   componentDidUpdate = () => {
     // console.log('UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUDGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH');
-    console.log(this.state.currTimeline);
+    // console.log(this.state.currTimeline);
     console.log(this.props.selected);
     // if (this.state.currTimeline !== this.props.selected) {
     console.log('UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUDGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH');
