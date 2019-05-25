723fb6403d3f7f8fdf1bf0c0d8b0ce9d02514bc4
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-05-25 14:40:09 -0400

    first go at making the add button fire the add action"

--- src/components/detail.js
+++ src/components/detail.js
@@ -13,7 +13,14 @@ class Detail extends Component {
     return (
       <div className="detailContainer">
         <div className="detailTitle">
-          {this.props.title}
+          <span>{this.props.title}</span>
+          <button
+            type="button"
+            className="buttonCTAwhite-alt"
+            onClick={this.props.add}
+          >
+          Save
+          </button>
         </div>
         <div className="detailImg">
           <img className="Img" src={this.props.cover_url} alt="Detail" />
