fc4d7463954b5536d9f67a9f94156833f100c9f9
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-05-29 16:16:21 -0400

    Possibly fixed date issue

--- src/components/add.js
+++ src/components/add.js
@@ -36,11 +36,13 @@ class AddForm extends Component {
   // set the initial state if told to in this.props.update
   componentWillMount() {
     if (this.props.update) {
+      console.log(typeof this.props.timeline.time);
+      const dt = new Date(this.props.timeline.time);
       this.setState({
         title: this.props.timeline.title,
         content: this.props.timeline.content,
         filter: this.props.timeline.filter,
-        time: this.props.timeline.time / (60 * 60 * 24 * 30 * 1000),
+        time: Math.round(dt.getTime() / (60 * 60 * 24 * 30 * 1000)),
         cover_url: this.props.timeline.cover_url,
       });
     }
@@ -209,7 +211,7 @@ class AddForm extends Component {
             placeholder="Time IN MONTHS SINCE HIGH SCHOOL"
             onChange={this.edit}
             onBlur={this.handleTagBlur}
-            value={this.state.time}
+            value={(this.state.time)}
           />
           {/* <em>* Required</em> */}
         </div>
